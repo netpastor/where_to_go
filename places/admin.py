51ee96779c36472a58483d23d14f7bfe5aa6868f
Vadim Shatalov <vad.shatalov@x5.ru>
2022-04-12 06:01:03 +0400

    Setup media files serving

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Place, PlaceImage
 
-# Register your models here.
+
+class PlaceImageInlineAdmin(admin.TabularInline):
+    model = PlaceImage
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    inlines = (PlaceImageInlineAdmin, )
+
+
+admin.site.register(Place, PlaceAdmin)
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -85,4 +85,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "_static"),
 ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
