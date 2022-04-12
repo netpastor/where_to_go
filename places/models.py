b09d870b199d94df484efbe7703cc679c8a3c0ba
Vadim Shatalov <vad.shatalov@x5.ru>
2022-04-12 05:27:24 +0400

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    coord_lng = models.CharField(max_length=20)
+    coord_lat = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = 'место'
+        verbose_name_plural = 'места'
+
+    def __str__(self):
+        return f'{self.pk} {self.title}'
