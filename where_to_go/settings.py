7361be12e930f40f1db44cce8efc207ec57f7d63
Vadim Shatalov <vad.shatalov@x5.ru>
2022-04-12 05:07:22 +0400

    Add frontend

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -79,5 +79,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "_static"),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
